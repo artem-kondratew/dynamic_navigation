e29f0ca5a0d0d31ecb7dfe340b495a4eb74a048a
artem-kondratew <artemkondratev5@gmail.com>
2024-05-10 17:02:19 +0300

    release for gazebo

--- data_synchronizer/src/data_synchronizer.cpp
+++ data_synchronizer/src/data_synchronizer.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <iostream>
 #include <memory>
 
@@ -7,49 +8,41 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <nav_msgs/msg/odometry.hpp>
-#include <sensor_msgs/msg/camera_info.hpp>
 #include <sensor_msgs/msg/image.hpp>
 
 #include <message_filters/subscriber.h>
 #include <message_filters/sync_policies/approximate_time.h>
 #include <message_filters/synchronizer.h>
 
-#include "rtabmap_msgs/msg/motion_detector_data.hpp"
-
-
-using std::placeholders::_1;
+#include "dynamic_nav_msgs/msg/yolo_data.hpp"
 
 
 class DataSyncronizer : public rclcpp::Node {
 private:
-    using odom_policy = message_filters::sync_policies::ApproximateTime<sensor_msgs::msg::CameraInfo,
-    sensor_msgs::msg::Image, sensor_msgs::msg::Image, nav_msgs::msg::Odometry>;
+    using odom_policy = message_filters::sync_policies::ApproximateTime<sensor_msgs::msg::Image, sensor_msgs::msg::Image, nav_msgs::msg::Odometry>;
 
-    using no_odom_policy = message_filters::sync_policies::ApproximateTime<sensor_msgs::msg::CameraInfo,
-    sensor_msgs::msg::Image, sensor_msgs::msg::Image>;
+    using no_odom_policy = message_filters::sync_policies::ApproximateTime<sensor_msgs::msg::Image, sensor_msgs::msg::Image>;
     
     std::unique_ptr<message_filters::Synchronizer<odom_policy>> odom_sync_;
     std::unique_ptr<message_filters::Synchronizer<no_odom_policy>> no_odom_sync_;
 
-    message_filters::Subscriber<sensor_msgs::msg::CameraInfo> camera_info_sub_;
     message_filters::Subscriber<sensor_msgs::msg::Image> rgb_sub_;
     message_filters::Subscriber<sensor_msgs::msg::Image> depth_sub_;
     message_filters::Subscriber<nav_msgs::msg::Odometry> odom_sub_;
 
-    rclcpp::Publisher<rtabmap_msgs::msg::MotionDetectorData>::SharedPtr output_pub_;
+    rclcpp::Publisher<dynamic_nav_msgs::msg::YoloData>::SharedPtr output_pub_;
+    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr rgb_vis_pub_;
 
 public:
     DataSyncronizer();
 
 private:
     void odomCallback(
-        const sensor_msgs::msg::CameraInfo::ConstSharedPtr camera_info,
         const sensor_msgs::msg::Image::ConstSharedPtr rgb,
         const sensor_msgs::msg::Image::ConstSharedPtr depth,
         const nav_msgs::msg::Odometry::ConstSharedPtr odom
     );
-    void callback(
-        const sensor_msgs::msg::CameraInfo::ConstSharedPtr camera_info,
+    void noOdomCallback(
         const sensor_msgs::msg::Image::ConstSharedPtr rgb,
         const sensor_msgs::msg::Image::ConstSharedPtr depth
     );
@@ -57,89 +50,86 @@ private:
 
 
 DataSyncronizer::DataSyncronizer() : Node("data_synchronizer") {
-    this->declare_parameter("camera_info_input", "");
-    this->declare_parameter("rgb_input", "");
-    this->declare_parameter("depth_input", "");
-    this->declare_parameter("odom_input", "");
-    this->declare_parameter("output", "");
+    this->declare_parameter("rgb_topic", "");
+    this->declare_parameter("depth_topic", "");
+    this->declare_parameter("odom_topic", "");
+    this->declare_parameter("output_topic", "");
+    this->declare_parameter("rgb_vis_topic", "");
     this->declare_parameter("use_odom", true);
 
-    std::string camera_info_topic = this->get_parameter("camera_info_input").as_string();
-    std::string rgb_topic = this->get_parameter("rgb_input").as_string();
-    std::string depth_topic = this->get_parameter("depth_input").as_string();
-    std::string odom_topic = this->get_parameter("odom_input").as_string();
-    std::string output_topic = this->get_parameter("output").as_string();
+    std::string rgb_topic = this->get_parameter("rgb_topic").as_string();
+    std::string depth_topic = this->get_parameter("depth_topic").as_string();
+    std::string odom_topic = this->get_parameter("odom_topic").as_string();
+    std::string output_topic = this->get_parameter("output_topic").as_string();
+    std::string rgb_vis_topic = this->get_parameter("rgb_vis_topic").as_string();
     bool use_odom = this->get_parameter("use_odom").as_bool();
 
-    RCLCPP_INFO(this->get_logger(), "camera_info_topic: '%s'", camera_info_topic.c_str());
     RCLCPP_INFO(this->get_logger(), "rgb_topic: '%s'", rgb_topic.c_str());
     RCLCPP_INFO(this->get_logger(), "depth_topic: '%s'", depth_topic.c_str());
     RCLCPP_INFO(this->get_logger(), "odom_topic: '%s'", odom_topic.c_str());
     RCLCPP_INFO(this->get_logger(), "output_topic: '%s'", output_topic.c_str());
+    RCLCPP_INFO(this->get_logger(), "rgb_vis_topic: '%s'", rgb_vis_topic.c_str());
     RCLCPP_INFO(this->get_logger(), "use_odom: '%s'", (use_odom ? "true" : "false"));
 
-    camera_info_sub_.subscribe(this, camera_info_topic);
-    rgb_sub_.subscribe(this, rgb_topic);
-    depth_sub_.subscribe(this, depth_topic);
+    rmw_qos_profile_t qos = rmw_qos_profile_default;
+    qos.depth = 1;
+    qos.reliability = rmw_qos_reliability_policy_t::RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT;
+    qos.history = rmw_qos_history_policy_t::RMW_QOS_POLICY_HISTORY_KEEP_LAST;
+    qos.durability = rmw_qos_durability_policy_t::RMW_QOS_POLICY_DURABILITY_VOLATILE;
+
+    rgb_sub_.subscribe(this, rgb_topic, qos);
+    depth_sub_.subscribe(this, depth_topic, qos);
     if (use_odom) {
         odom_sub_.subscribe(this, odom_topic);
-        odom_sync_.reset(new message_filters::Synchronizer<odom_policy>(odom_policy(10), camera_info_sub_, rgb_sub_, depth_sub_, odom_sub_));
-        odom_sync_->registerCallback(std::bind(&DataSyncronizer::odomCallback, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4));
+        odom_sync_.reset(new message_filters::Synchronizer<odom_policy>(odom_policy(10), rgb_sub_, depth_sub_, odom_sub_));
+        odom_sync_->registerCallback(std::bind(&DataSyncronizer::odomCallback, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
     }
     else {
-        no_odom_sync_.reset(new message_filters::Synchronizer<no_odom_policy>(no_odom_policy(10), camera_info_sub_, rgb_sub_, depth_sub_));
-        no_odom_sync_->registerCallback(std::bind(&DataSyncronizer::callback, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
+        no_odom_sync_.reset(new message_filters::Synchronizer<no_odom_policy>(no_odom_policy(10), rgb_sub_, depth_sub_));
+        no_odom_sync_->registerCallback(std::bind(&DataSyncronizer::noOdomCallback, this, std::placeholders::_1, std::placeholders::_2));
 
     }
     
-    output_pub_ = this->create_publisher<rtabmap_msgs::msg::MotionDetectorData>(output_topic, 10);
+    output_pub_ = this->create_publisher<dynamic_nav_msgs::msg::YoloData>(output_topic, 10);
+    rgb_vis_pub_ = this->create_publisher<sensor_msgs::msg::Image>(rgb_vis_topic, 10);
 }
 
 
-void DataSyncronizer::odomCallback(const sensor_msgs::msg::CameraInfo::ConstSharedPtr camera_info, const sensor_msgs::msg::Image::ConstSharedPtr rgb,
+void DataSyncronizer::odomCallback(const sensor_msgs::msg::Image::ConstSharedPtr rgb,
                                const sensor_msgs::msg::Image::ConstSharedPtr depth, const nav_msgs::msg::Odometry::ConstSharedPtr odom) {
-    auto msg = rtabmap_msgs::msg::MotionDetectorData();
-    msg.camera_info = *camera_info;
+    auto start_timer = std::chrono::system_clock::now();
+
+    auto msg = dynamic_nav_msgs::msg::YoloData();
     msg.rgb = *rgb;
     msg.depth = *depth;
     msg.odom = *odom;
 
     output_pub_->publish(msg);
+    rgb_vis_pub_->publish(msg.rgb);
 
-    size_t w = msg.rgb.width;
-    size_t h = msg.rgb.height;
-    size_t size = w * h;
-    cv::Mat img(h, w, CV_8UC3);
-    std::memcpy(img.data, msg.rgb.data.data(), sizeof(uint8_t) * size * 3);
-
-    cv::imshow("data", img);
-    cv::waitKey(1);
-
-    RCLCPP_INFO(this->get_logger(), "sync with odom created: %u:%u %u:%u %u:%u", msg.rgb.header.stamp.sec, msg.rgb.header.stamp.nanosec,
-    msg.depth.header.stamp.sec, msg.depth.header.stamp.nanosec, msg.odom.header.stamp.sec, msg.odom.header.stamp.nanosec);
+    auto end_timer = std::chrono::system_clock::now();
+    auto dt = std::chrono::duration_cast<std::chrono::milliseconds>(end_timer - start_timer).count();
+    
+    RCLCPP_INFO(this->get_logger(), "sync with odom created: %u:%u %u:%u %u:%u; dt = %ldms", msg.rgb.header.stamp.sec, msg.rgb.header.stamp.nanosec,
+    msg.depth.header.stamp.sec, msg.depth.header.stamp.nanosec, msg.odom.header.stamp.sec, msg.odom.header.stamp.nanosec, dt);
 }
 
 
-void DataSyncronizer::callback(const sensor_msgs::msg::CameraInfo::ConstSharedPtr camera_info, const sensor_msgs::msg::Image::ConstSharedPtr rgb,
-                               const sensor_msgs::msg::Image::ConstSharedPtr depth) {
-    auto msg = rtabmap_msgs::msg::MotionDetectorData();
-    msg.camera_info = *camera_info;
+void DataSyncronizer::noOdomCallback(const sensor_msgs::msg::Image::ConstSharedPtr rgb, const sensor_msgs::msg::Image::ConstSharedPtr depth) {
+    auto start_timer = std::chrono::system_clock::now();
+
+    auto msg = dynamic_nav_msgs::msg::YoloData();
     msg.rgb = *rgb;
     msg.depth = *depth;
 
     output_pub_->publish(msg);
+    rgb_vis_pub_->publish(msg.rgb);
 
-    size_t w = msg.rgb.width;
-    size_t h = msg.rgb.height;
-    size_t size = w * h;
-    cv::Mat img(h, w, CV_8UC3);
-    std::memcpy(img.data, msg.rgb.data.data(), sizeof(uint8_t) * size * 3);
-
-    cv::imshow("data", img);
-    cv::waitKey(1);
-
-    RCLCPP_INFO(this->get_logger(), "sync without odom created: %u:%u %u:%u", msg.rgb.header.stamp.sec, msg.rgb.header.stamp.nanosec,
-    msg.depth.header.stamp.sec, msg.depth.header.stamp.nanosec);
+    auto end_timer = std::chrono::system_clock::now();
+    auto dt = std::chrono::duration_cast<std::chrono::milliseconds>(end_timer - start_timer).count();
+    
+    RCLCPP_INFO(this->get_logger(), "sync without odom created: %u:%u %u:%u; dt = %ldms", msg.rgb.header.stamp.sec, msg.rgb.header.stamp.nanosec,
+    msg.depth.header.stamp.sec, msg.depth.header.stamp.nanosec, dt);
 }
 
 
