d27c6148c10a1181fb0aa8f73cd30a5dad3221d5
artem-kondratew <artemkondratev5@gmail.com>
2024-05-11 22:51:15 +0300

    add custom rtabmap launch file

---
+++ dynamic_navigation/launch/rtabmap.launch.py
@@ -0,0 +1,58 @@
+# Requirements:
+#   A realsense D435i
+#   Install realsense2 ros2 package (ros-$ROS_DISTRO-realsense2-camera)
+# Example:
+#   $ ros2 launch realsense2_camera rs_launch.py enable_gyro:=true enable_accel:=true unite_imu_method:=1 enable_sync:=true align_depth.enable
+#
+#   $ ros2 launch rtabmap_examples realsense_d435i_color.launch.py
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    parameters=[{
+          'frame_id':'camera_link',
+          'subscribe_depth':True,
+          'subscribe_odom_info':False,
+          'approx_sync':False,
+          'wait_imu_to_init':True,}]
+
+    remappings=[
+          ('imu', '/imu/data'),
+          ('rgb/image', '/rtabmap/yolo/rgb'),
+          ('rgb/camera_info', '/rtabmap/yolo/camera_info'),
+          ('depth/image', '/rtabmap/yolo/depth')]
+
+    return LaunchDescription([
+
+        # Nodes to launch       
+        Node(
+            package='rtabmap_odom', executable='rgbd_odometry', output='screen',
+            parameters=parameters,
+            remappings=remappings),
+
+        Node(
+            package='rtabmap_slam', executable='rtabmap', output='screen',
+            parameters=parameters,
+            remappings=remappings,
+            arguments=['-d']),
+
+        Node(
+            package='rtabmap_viz', executable='rtabmap_viz', output='screen',
+            parameters=parameters,
+            remappings=remappings),
+
+        Node(
+            package='imu_filter_madgwick', executable='imu_filter_madgwick_node', output='screen',
+            parameters=[{'use_mag': False, 
+                         'world_frame':'enu', 
+                         'publish_tf':False}],
+            remappings=[('imu/data_raw', '/camera/imu')]),
+        
+        # The IMU frame is missing in TF tree, add it:
+        Node(
+            package='tf2_ros', executable='static_transform_publisher', output='screen',
+            arguments=['0', '0', '0', '0', '0', '0', 'camera_gyro_optical_frame', 'camera_imu_optical_frame']),
+    ])
